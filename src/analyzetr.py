9cbe4c522b4b96a58a9c168e05c90eff1b0f38c8
bmouraz <bmouraz@live.com>
2020-05-26 14:26:50 -0300

    Version 0.2

--- src/analyzetr.py
+++ src/analyzetr.py
@@ -5,7 +5,6 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 def analyzer(filename):
-
     lines = open(filename).readlines()
     lines=[x.split() for x in lines]
 
@@ -39,21 +38,17 @@ def analyzer(filename):
     jitterrec = []
     jitterSum = []
 
-
-    for i in range (len(lines)):
-
-
+    for i in range(len(lines)):
         if lines[i][0] == "+":
             timep = float(lines[i][1])
 
             if pcktloss > 0:
 
                 rtimes = 0
-
-
         elif lines[i][0] == "-":
             timem = float(lines[i][1])
             queuedelay = timem - timep
+
         else:
             #Received time
             timerec = float(lines[i][1])
@@ -63,11 +58,17 @@ def analyzer(filename):
             res = list(map(int, temp))
             node = res[0]
 
+            if not int(node) in nodelist:
+                if int(node) < (len(nodelist)):
+                    nodelist[int(node)] = int(node)
+                    listrec[int(node)] = 1
+                else:
+                    for x in range (len(nodelist)-1,int(node)):
+                        nodelist.append(0)
+                        listrec.append(0)
+                    nodelist[int(node)] = int(node)
+                    listrec[int(node)] = 1
 
-            if not node in nodelist:
-
-                nodelist.append(int(node))
-                listrec.append(1)
                 if (int(node)) != 0 and (len(jitter)) < (int(node))+1:
                     for i in range (int(node)+1):
                         jitter.append(0)
@@ -78,20 +79,31 @@ def analyzer(filename):
 
 
                 if lines[i][31] == "ns3::UdpHeader":
-                    sentdatarec.append(int(lines[i][33]))
+                    if int(node) < (len(sentdatarec)):
+                        sentdatarec[int(node)] = int(lines[i][33])
+                    else:
+                        for x in range (len(sentdatarec)-1,int(node)):
+                            sentdatarec.append(0)
+                        sentdatarec[int(node)] = int(lines[i][33])
                 else:
-                    sentdatarec.append(int(lines[i][27]))
-            else:
+                    if int(node) < (len(sentdatarec)):
+                        sentdatarec[int(node)] = int(lines[i][27])
+                    else:
+                        for x in range (len(sentdatarec)-1,int(node)):
+                            sentdatarec.append(0)
+                        sentdatarec[int(node)] = int(lines[i][27])
 
-                listrec[int(node)-1] = listrec[int(node)-1] + 1
+
+            else:
+                listrec[int(node)] = listrec[int(node)] + 1
+
                 if lines[i][31] == "ns3::UdpHeader":
-                    # print ("node{}: sent {}" .format(node,lines[i][33]) )
+
                     sentdatarec[int(node)] = sentdatarec[int(node)] + int(lines[i][33])
                 else:
-                    # print ("node{}: sent {}" .format(node,lines[i][27]) )
+
                     sentdatarec[int(node)] = sentdatarec[int(node)] + int(lines[i][27])
 
-                # print(jitter, timerec, node)
                 jitter[int(node)] = - (jitter[int(node)] - timerec)
                 jitterSum[int(node)] = jitterSum[int(node)] + jitter[int(node)]
                 jitterrec[int(node)] = jitter[int(node)]
@@ -106,28 +118,15 @@ def analyzer(filename):
                 #if >1 has packet loss
                 if nodenext == node and lines[i+1][0] == lines[i][0]:
                     pcktloss += 1
-                    # print(pcktloss)
 
-            try:
-
-                lambdacalc = ((float(sentdatarec[int(node)]))/float(sizelength))/(float(tmpSimu))
-                en = 0
-                ew = 0
-                throughput = lambdacalc*(float(totallength)+2)
-                goodput = lambdacalc*((float(sentdatarec[node]))/listrec[node])
-                droppedpackets = pcktloss
-                stringcsv =("{},{},{},{},{},{},{},{},{},{},{},{}\n" .format(timerec,nodelist[int(node)],lambdacalc,en,ew,en*lambdacalc,throughput,goodput,droppedpackets,sentdatarec[int(node)]/1000,jitterrec[int(node)],jitterSum[node]))
-                completecsvfile.write(stringcsv)
-            except:
-
-                lambdacalc = ((float(sentdatarec[int(0)]))/float(sizelength))/(float(tmpSimu))
-                en = 0
-                ew = 0
-                throughput = lambdacalc*(float(totallength)+2)
-                goodput = lambdacalc*((float(sentdatarec[int(0)]))/listrec[0])
-                droppedpackets = pcktloss
-                stringcsv =("{},{},{},{},{},{},{},{},{},{},{},{}\n" .format(timerec,nodelist[0],lambdacalc,en,ew,en*lambdacalc,throughput,goodput,droppedpackets,sentdatarec[0]/1000,jitterrec[0],jitterSum[0]))
-                completecsvfile.write(stringcsv)
+            lambdacalc = ((float(sentdatarec[int(node)]))/float(sizelength))/(float(tmpSimu))
+            en = 0
+            ew = 0
+            throughput = lambdacalc*(float(totallength)+2)
+            goodput = lambdacalc*((float(sentdatarec[node]))/listrec[node])
+            droppedpackets = pcktloss
+            stringcsv =("{},{},{},{},{},{},{},{},{},{},{},{}\n" .format(timerec,nodelist[int(node)],lambdacalc,en,ew,en*lambdacalc,throughput,goodput,droppedpackets,sentdatarec[int(node)]/1000,jitterrec[int(node)],jitterSum[node]))
+            completecsvfile.write(stringcsv)
 
     for i in range (len(nodelist)):
         lambdacalc = ((float(sentdatarec[i]))/float(sizelength))/(float(tmpSimu))
@@ -137,5 +136,4 @@ def analyzer(filename):
         goodput = lambdacalc*(float(sentdatarec[i]/listrec[i]))
         droppedpackets = pcktloss
         stringcsv =("{},{},{},{},{},{},{},{},{},{}\n" .format(nodelist[i],lambdacalc,en,ew,en*lambdacalc,throughput,goodput,droppedpackets,sentdatarec[i]/1000,jitterSum[i]))
-        # print(stringcsv)
         csvfile.write(stringcsv)
